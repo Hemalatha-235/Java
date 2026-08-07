16217d483a0e8d4d3f608ce8ba9cffc9570a58d4
Hemalatha-235 <haarikamatcha@gmail.com>
2026-08-07 18:25:17 +0530

    Add files via upload

---
+++ Practice Programs/Constructor/ConDemo.java
@@ -0,0 +1,32 @@
+package javacore;
+
+public class ConDemo {
+// class or instance variables
+	int rollNo;
+    String name;
+    float fee;
+	
+	// constructor 
+	public ConDemo() { 
+	 this.rollNo = 235;
+	 this.name= "Hema";
+	 this.fee= 5000.98F;
+	}
+	void sample(){
+		System.out.println("ROLL NO is:" + this.rollNo);
+		System.out.println("NAME is:" + this.name);
+		System.out.println("FEE is:" + this.fee);
+	}
+	void example() {
+		System.out.println("some other method named example");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This is main method..!");
+		ConDemo cd = new ConDemo();
+		cd.sample();
+		cd.example();
+	}
+		
+	}
