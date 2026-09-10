d69199f80ac9ca3ef5473fcc7fa87032792d566f
Hemalatha-235 <haarikamatcha@gmail.com>
2026-09-10 21:39:52 +0530

    Update StringClassOps.java

--- Assignment Programs/Assignment 03/StringClassOps.java
+++ Assignment Programs/Assignment 03/StringClassOps.java
@@ -37,28 +37,72 @@ public class StringClassOps {
             sc.nextLine();
 
             switch (choice) {
-                case 1: System.out.println(str.length()); break;
-                case 2: System.out.println(str.charAt(4)); break;
-                case 3: System.out.println(str.substring(0,5)); break;
-                case 4: System.out.println(str.contains("World")); break;
-                case 5: System.out.println(str.equals(str2)); break;
-                case 6: System.out.println(str.equalsIgnoreCase("hello world")); break;
-                case 7: System.out.println(str.compareTo(str2)); break;
-                case 8: System.out.println(str.startsWith("Hello")); break;
-                case 9: System.out.println(str.endsWith("World")); break;
-                case 10: System.out.println(str.indexOf('o')); break;
-                case 11: System.out.println(str.lastIndexOf('o')); break;
-                case 12: System.out.println(str.toUpperCase()); break;
-                case 13: System.out.println(str.toLowerCase()); break;
-                case 14: System.out.println("   Java   ".trim()); break;
-                case 15: System.out.println(str.replace("World","Java")); break;
-                case 16: for(String s: str2.split(" ")) System.out.println(s); break;
-                case 17: System.out.println(String.join("-", "Java","is","fun")); break;
-                case 18: System.out.println(str.concat(" " + str2)); break;
-                case 19: System.out.println("".isEmpty()); break;
-                case 20: System.out.println(String.valueOf(123)); break;
-                case 0: System.out.println("Exit"); return;
-                default: System.out.println("Invalid!");
+                case 1:
+                    System.out.println(str.length()); 
+                    break;
+                case 2: 
+                    System.out.println(str.charAt(4));
+                    break;
+                case 3: 
+                    System.out.println(str.substring(0,5));
+                    break;
+                case 4:
+                    System.out.println(str.contains("World"));
+                    break;
+                case 5:
+                    System.out.println(str.equals(str2));
+                    break;
+                case 6:
+                    System.out.println(str.equalsIgnoreCase("hello world"));
+                    break;
+                case 7:
+                    System.out.println(str.compareTo(str2));
+                    break;
+                case 8:
+                    System.out.println(str.startsWith("Hello"));
+                    break;
+                case 9:
+                    System.out.println(str.endsWith("World"));
+                    break;
+                case 10:
+                    System.out.println(str.indexOf('o'));
+                    break;
+                case 11:
+                    System.out.println(str.lastIndexOf('o'));
+                    break;
+                case 12:
+                    System.out.println(str.toUpperCase());
+                    break;
+                case 13:
+                    System.out.println(str.toLowerCase());
+                    break;
+                case 14:
+                    System.out.println("   Java   ".trim());
+                    break;
+                case 15:
+                    System.out.println(str.replace("World","Java"));
+                    break;
+                case 16:
+                    for(String s: str2.split(" "))
+                    System.out.println(s); 
+                    break;
+                case 17:
+                    System.out.println(String.join("-", "Java","is","fun"));
+                    break;
+                case 18:
+                    System.out.println(str.concat(" " + str2));
+                    break;
+                case 19:
+                    System.out.println("".isEmpty());
+                    break;
+                case 20:
+                    System.out.println(String.valueOf(123));
+                    break;
+                case 0:
+                    System.out.println("Exit");
+                    return;
+                default:
+                    System.out.println("Invalid!");
             }
         }
     }
